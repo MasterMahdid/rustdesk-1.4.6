2e00fabfef27301c38e422a018484b5dd927dac5
Master Mahdi <mahdidaneshvar.aka.haskell@gmail.com>
2026-05-02 01:19:19 +0330

    update

--- flutter/ndk_arm.sh
+++ flutter/ndk_arm.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+export VCPKG_ROOT=/opt/vcpkg
+export PKG_CONFIG_PATH="$VCPKG_ROOT/installed/arm-android/lib/pkgconfig"
+export PKG_CONFIG_LIBDIR="$PKG_CONFIG_PATH"
+export PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1
+export C_INCLUDE_PATH="$VCPKG_ROOT/installed/arm-android/include"
+export CPLUS_INCLUDE_PATH="$C_INCLUDE_PATH"
+export LIBRARY_PATH="$VCPKG_ROOT/installed/arm-android/lib"
 cargo ndk --platform 21 --target armv7-linux-androideabi build --release --features flutter,hwcodec
--- flutter/ndk_arm64.sh
+++ flutter/ndk_arm64.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+export VCPKG_ROOT=/opt/vcpkg
+export PKG_CONFIG_PATH="$VCPKG_ROOT/installed/arm64-android/lib/pkgconfig"
+export PKG_CONFIG_LIBDIR="$PKG_CONFIG_PATH"
+export PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1
+export C_INCLUDE_PATH="$VCPKG_ROOT/installed/arm64-android/include"
+export CPLUS_INCLUDE_PATH="$C_INCLUDE_PATH"
+export LIBRARY_PATH="$VCPKG_ROOT/installed/arm64-android/lib"
 cargo ndk --platform 21 --target aarch64-linux-android build --release --features flutter,hwcodec
