17791045289f96937af69d0d16816ac1269923ce
Master Mahdi <mahdidaneshvar.aka.haskell@gmail.com>
2026-05-09 20:35:54 +0330

    trying to fix x64 build

--- flutter/ndk_arm64.sh
+++ flutter/ndk_arm64.sh
@@ -3,9 +3,9 @@
 #xport PKG_CONFIG_PATH="$VCPKG_ROOT/installed/arm64-android/lib/pkgconfig"
 #export PKG_CONFIG_LIBDIR="$PKG_CONFIG_PATH"
 #export PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1
-#export C_INCLUDE_PATH="$VCPKG_ROOT/installed/arm64-android/include"
-#export CPLUS_INCLUDE_PATH="$C_INCLUDE_PATH"
-#export LIBRARY_PATH="$VCPKG_ROOT/installed/arm64-android/lib"
+export C_INCLUDE_PATH="$VCPKG_ROOT/installed/arm64-android/include"
+export CPLUS_INCLUDE_PATH="$C_INCLUDE_PATH"
+export LIBRARY_PATH="$VCPKG_ROOT/installed/arm64-android/lib"
 #cargo ndk --platform 21 --target aarch64-linux-android build --release --features flutter,hwcodec
 
 
