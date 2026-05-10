770a3ca96ef9e2801deca35e10a50d690f7b1b91
Master Mahdi <mahdidaneshvar.aka.haskell@gmail.com>
2026-05-10 17:28:30 +0330

    Yekan font

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -373,6 +373,7 @@ class MyTheme {
 
   static ThemeData lightTheme = ThemeData(
     // https://stackoverflow.com/questions/77537315/after-upgrading-to-flutter-3-16-the-app-bar-background-color-button-size-and
+    fontFamily: 'Yekan'
     useMaterial3: false,
     brightness: Brightness.light,
     hoverColor: Color.fromARGB(255, 224, 224, 224),
